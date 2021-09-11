08337510900355cba68055187d3314e019caa950
fernando gomes <fernando.gmsoli@gmail.com>
2021-09-10 21:09:43 -0300

    Created repository

---
+++ app/src/main/java/com/fgomes/subscribers/repository/DatabaseDataSource.kt
@@ -0,0 +1,39 @@
+package com.fgomes.subscribers.repository
+
+import androidx.lifecycle.LiveData
+import com.fgomes.subscribers.data.db.dao.SubscriberDao
+import com.fgomes.subscribers.data.db.entity.SubscriberEntity
+
+class DatabaseDataSource(
+    private val subscriberDao: SubscriberDao
+) : SubscriberRepository {
+    override suspend fun insertSubscriber(name: String, email: String): Long {
+        val subscriber = SubscriberEntity(
+            name = name,
+            email = email
+        )
+        return subscriberDao.insert(subscriber)
+    }
+
+    override suspend fun updateSubscriber(id: Long, name: String, email: String) {
+        val subscriber = SubscriberEntity(
+            id = id,
+            name = name,
+            email = email
+        )
+        return subscriberDao.update(subscriber)
+    }
+
+    override suspend fun deleteSubscriber(id: Long) {
+        subscriberDao.delete(id)
+    }
+
+    override suspend fun deleteAllSubscriber() {
+        subscriberDao.deleteAll()
+    }
+
+    override suspend fun getAllSubscriber(): LiveData<List<SubscriberEntity>> {
+        return subscriberDao.getAll()
+    }
+
+}
---
+++ app/src/main/java/com/fgomes/subscribers/repository/SubscriberRepository.kt
@@ -0,0 +1,13 @@
+package com.fgomes.subscribers.repository
+
+import androidx.lifecycle.LiveData
+import com.fgomes.subscribers.data.db.entity.SubscriberEntity
+
+interface SubscriberRepository {
+
+    suspend fun insertSubscriber (name: String, email: String): Long
+    suspend fun updateSubscriber (id: Long, name: String, email: String)
+    suspend fun deleteSubscriber (id: Long)
+    suspend fun deleteAllSubscriber ()
+    suspend fun getAllSubscriber (): LiveData<List<SubscriberEntity>>
+}
