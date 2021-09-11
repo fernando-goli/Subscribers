549c28dfe71230f9e69185cee6b24c04abeca92a
fernando gomes <fernando.gmsoli@gmail.com>
2021-09-10 21:13:09 -0300

    Created navigation jetpack

--- app/src/main/java/com/fgomes/subscribers/MainActivity.kt
+++ app/src/main/java/com/fgomes/subscribers/MainActivity.kt
@@ -2,12 +2,24 @@ package com.fgomes.subscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var app_toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        app_toolbar = findViewById(R.id.app_toolbar)
+        app_toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
 }
